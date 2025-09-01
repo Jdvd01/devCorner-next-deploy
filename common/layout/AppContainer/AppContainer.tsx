049719d9609a3ffad79449e0532e3317def004d4
Jose Velasquez <velasquezdenorajose@gmail.com>
2025-09-01 15:39:29 -0400

    feat(component): add AppContainer component

---
+++ common/layout/AppContainer/AppContainer.tsx
@@ -0,0 +1,32 @@
+import React, { createElement } from 'react'
+import styles from './AppContainer.module.css'
+import { BG_STYLES_CLASSES } from '@/common/utils/enums'
+
+type PropsType = {
+  bgColor?: BG_STYLES_CLASSES
+  className?: string
+  children: React.ReactNode | React.ReactNode[]
+  elementType?: string
+  padding?: boolean
+}
+
+const AppContainer = ({
+  bgColor = BG_STYLES_CLASSES.LIGHT,
+  elementType = 'main',
+  className = '',
+  padding = false,
+  children,
+}: PropsType) => {
+  const element = createElement(
+    elementType,
+    {
+      className: `${className} ${styles.app_container} ${
+        padding ? styles.padding : ''
+      }`,
+    },
+    children
+  )
+  return <div className={`${styles.app_main_box} ${bgColor}`}>{element}</div>
+}
+
+export default AppContainer
